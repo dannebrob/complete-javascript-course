bc43a7d7e70868bf4ec2b57e67b693b68a1a8daf
dannebrob <65211641+dannebrob@users.noreply.github.com>
2022-06-23 20:13:41 +0200

    update

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,72 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map;
+let mapEvent;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (postion) {
+      const { latitude } = postion.coords;
+      const { longitude } = postion.coords;
+      const coords = [latitude, longitude]; //user cords
+      //succes callback
+      map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+      //handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        console.log(mapEvent); //maps out where on the map the click happed
+
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    function () {
+      //Error callback
+      alert('Error, could not get your postion');
+    }
+  );
+}
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //clear input fields:
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  //Display marker
+  const { lat, lng } = mapEvent.latlng;
+  console.log(lat, lng);
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        closeOnClick: false,
+        autoClose: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('workout')
+    .openPopup();
+});
+//Chage between running and cycling
+inputType,
+  addEventListener('change', function () {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  });
